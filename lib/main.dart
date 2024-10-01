46098d3e69c80478cc475ff4f96003e6c2ddbc63
RenatBakaiev <renattesttask@gmail.com>
2024-10-01 17:31:57 +0300

    CRYPTO: TALKER BLOC LOGGER - init in main

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,11 @@ import 'package:crypto_coins_list/crypto_coins_list_app.dart';
 import 'package:crypto_coins_list/repositories/crypto_coins/crypto_coins.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get_it/get_it.dart';
 import 'package:talker_dio_logger/talker_dio_logger.dart';
 import 'package:talker_flutter/talker_flutter.dart';
+import 'package:talker_bloc_logger/talker_bloc_logger.dart';
 
 void main() {
   final talker = TalkerFlutter.init();
@@ -21,6 +23,10 @@ void main() {
         printResponseData: false,
       ),
     ),
+  );
+
+  Bloc.observer = TalkerBlocObserver(
+    talker: talker,
   );
 
   GetIt.I.registerLazySingleton<AbstractCoinsRepository>(
