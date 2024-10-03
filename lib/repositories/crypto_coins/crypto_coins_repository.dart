c7b0e47d0b566b6996a1aba3a2d5dd6cbfd406cf
RenatBakaiev <renattesttask@gmail.com>
2024-10-03 11:33:57 +0300

    CRYPTO: JSON - clean code

--- lib/repositories/crypto_coins/crypto_coins_repository.dart
+++ lib/repositories/crypto_coins/crypto_coins_repository.dart
@@ -12,24 +12,14 @@ class CryptoCoinsRepository implements AbstractCoinsRepository {
   Future<List<CryptoCoin>> getCoinsList() async {
     final response = await dio.get(
         'https://min-api.cryptocompare.com/data/pricemultifull?fsyms=BTC,ETH,BNB,HMSTR,TON,DOGE,USDT,SOL&tsyms=USD');
-    // debugPrint(response.toString());
 
     final data = response.data as Map<String, dynamic>;
     final dataRaw = data['RAW'] as Map<String, dynamic>;
     final cryptoCoinsList = dataRaw.entries.map((e) {
       final usdData =
           (e.value as Map<String, dynamic>)['USD'] as Map<String, dynamic>;
-
       final details = CryptoCoinDetail.fromJson(usdData);
-
-      // final price = usdData['PRICE'];
-      // final imageUrl = usdData['IMAGEURL'];
-      return CryptoCoin(
-        name: e.key,
-        details: details,
-        // priceInUSD: price,
-        // imageUrl: 'https://www.cryptocompare.com/$imageUrl',
-      );
+      return CryptoCoin(name: e.key, details: details);
     }).toList();
     return cryptoCoinsList;
   }
@@ -44,19 +34,6 @@ class CryptoCoinsRepository implements AbstractCoinsRepository {
     final coinData = dataRaw[currencyCode] as Map<String, dynamic>;
     final usdData = coinData['USD'] as Map<String, dynamic>;
     final details = CryptoCoinDetail.fromJson(usdData);
-    // final price = usdData['PRICE'];
-    // final imageUrl = usdData['IMAGEURL'];
-    // final toSymbol = usdData['TOSYMBOL'];
-    // final lastUpdate = usdData['LASTUPDATE'];
-    // final hight24Hour = usdData['HIGH24HOUR'];
-    // final low24Hours = usdData['LOW24HOUR'];
-
     return CryptoCoin(name: currencyCode, details: details);
-    // priceInUSD: price,
-    // imageUrl: 'https://www.cryptocompare.com/$imageUrl',
-    // toSymbol: toSymbol,
-    // lastUpdate: DateTime.fromMillisecondsSinceEpoch(lastUpdate),
-    // hight24Hour: hight24Hour,
-    // low24Hours: low24Hours,
   }
 }
