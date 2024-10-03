86834883228e560735b1a1022da61c8dba7dcd54
RenatBakaiev <renattesttask@gmail.com>
2024-10-03 11:28:24 +0300

    CRYPTO: JSON - generate model, make changes in pages

--- lib/repositories/crypto_coins/abstract_coins_repository.dart
+++ lib/repositories/crypto_coins/abstract_coins_repository.dart
@@ -1,8 +1,7 @@
 import 'package:crypto_coins_list/repositories/crypto_coins/models/crypto_coin.dart';
-import 'package:crypto_coins_list/repositories/crypto_coins/models/crypto_coin_details.dart';
 
 // It is our interface
 abstract class AbstractCoinsRepository {
   Future<List<CryptoCoin>> getCoinsList();
-  Future<CryptoCoinDetail> getCoinDetails(String currencyCode);
+  Future<CryptoCoin> getCoinDetails(String currencyCode);
 }
