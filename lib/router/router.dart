321634504d58ececf79b1314d0886a18b2cfac18
RenatBakaiev <renattesttask@gmail.com>
2024-10-04 11:08:13 +0300

    CRYPTO: AUTO-ROUTE - generate auto routes, remove old manual routes

--- lib/router/router.dart
+++ lib/router/router.dart
@@ -1,7 +1,14 @@
+import 'package:auto_route/auto_route.dart';
 import 'package:crypto_coins_list/features/crypto_coin/crypto_coin.dart';
 import 'package:crypto_coins_list/features/crypto_list/crypto_list.dart';
 
-final routes = {
-  '/': (context) => const CryptoListScreen(),
-  '/coin': (context) => const CryptoCoinScreen(),
-};
+part 'router.gr.dart';
+
+@AutoRouterConfig()
+class AppRouter extends _$AppRouter {
+  @override
+  List<AutoRoute> get routes => [
+        AutoRoute(page: CryptoListRoute.page, path: '/'),
+        AutoRoute(page: CryptoCoinRoute.page),
+      ];
+}
---
+++ lib/router/router.gr.dart
@@ -0,0 +1,59 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// AutoRouterGenerator
+// **************************************************************************
+
+// ignore_for_file: type=lint
+// coverage:ignore-file
+
+part of 'router.dart';
+
+abstract class _$AppRouter extends RootStackRouter {
+  // ignore: unused_element
+  _$AppRouter({super.navigatorKey});
+
+  @override
+  final Map<String, PageFactory> pagesMap = {
+    CryptoCoinRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const CryptoCoinScreen(),
+      );
+    },
+    CryptoListRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const CryptoListScreen(),
+      );
+    },
+  };
+}
+
+/// generated route for
+/// [CryptoCoinScreen]
+class CryptoCoinRoute extends PageRouteInfo<void> {
+  const CryptoCoinRoute({List<PageRouteInfo>? children})
+      : super(
+          CryptoCoinRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'CryptoCoinRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [CryptoListScreen]
+class CryptoListRoute extends PageRouteInfo<void> {
+  const CryptoListRoute({List<PageRouteInfo>? children})
+      : super(
+          CryptoListRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'CryptoListRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
