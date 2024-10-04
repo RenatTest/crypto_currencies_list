cbd8bc74a68a99f75f70347f9e97208941be8dce
RenatBakaiev <renattesttask@gmail.com>
2024-10-04 19:13:47 +0300

    CRYPTO: CASH IMAGES - cryptocoin list images

--- lib/features/crypto_list/widgets/crypto_coin_tile.dart
+++ lib/features/crypto_list/widgets/crypto_coin_tile.dart
@@ -1,4 +1,5 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:crypto_coins_list/repositories/crypto_coins/models/crypto_coin.dart';
 import 'package:crypto_coins_list/router/router.dart';
 import 'package:flutter/material.dart';
@@ -17,7 +18,8 @@ class CryptoCoinTile extends StatelessWidget {
     final theme = Theme.of(context);
     final coinDetails = coin.details;
     return ListTile(
-      leading: Image.network(coinDetails.fullImageUrl),
+      leading: CoinImage(fullImageUrl: coinDetails.fullImageUrl),
+      // leading: Image.network(coinDetails.fullImageUrl),
       title: Text(
         coin.name,
         style: theme.textTheme.bodyMedium,
@@ -30,6 +32,32 @@ class CryptoCoinTile extends StatelessWidget {
       onTap: () {
         AutoRouter.of(context).push(CryptoCoinRoute(coin: coin));
       },
+    );
+  }
+}
+
+class CoinImage extends StatelessWidget {
+  const CoinImage({
+    super.key,
+    required this.fullImageUrl,
+  });
+
+  final String fullImageUrl;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 50,
+      height: 50,
+      child: CachedNetworkImage(
+        imageUrl: fullImageUrl,
+        imageBuilder: (context, imageProvider) => Image(image: imageProvider),
+        progressIndicatorBuilder: (context, url, progress) => Center(
+          child: CircularProgressIndicator(
+            value: progress.progress,
+          ),
+        ),
+      ),
     );
   }
 }
